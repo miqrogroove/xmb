800ad18bc1648fbcd7b64756d0f90a8bbb7d0f54
mark <>
2007-07-14 21:55:32 +0000

    Put pollopts back. my bad
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code@33 4328ac3d-c443-4ce5-a636-224d0ca14295

--- source/install/cinst.php
+++ source/install/cinst.php
@@ -522,6 +522,7 @@ show_act("Creating ".$tablepre."threads");
           `author` varchar(32) NOT NULL default '',
           `closed` varchar(15) NOT NULL default '',
           `topped` tinyint(1) NOT NULL default 0,
+          `pollopts` tinyint(1) NOT NULL default 0,
           PRIMARY KEY  (`tid`),
           KEY `fid` (`fid`),
           KEY `tid` (`tid`),
