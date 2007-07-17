1ba4a8b9a05755c37892724fe98879dbafd12d3b
ixan <>
2007-07-17 07:13:23 +0000

    Check-all-javascript fix
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code@71 4328ac3d-c443-4ce5-a636-224d0ca14295

--- source/js/u2uheader.js
+++ source/js/u2uheader.js
@@ -3,31 +3,43 @@ var aBookLink = '';
 self.name = 'u2u';
 
 function setCheckboxes(the_form, do_check) {
-    var elts      = document.forms[the_form].elements['u2u_select[]']
-    var elts_cnt  = elts.length;
+	if (document.forms[the_form].elements['u2u_select[]']) {
+		var elts      = document.forms[the_form].elements['u2u_select[]'];
+		var elts_cnt  = elts.length;
 
-    if (elts_cnt) {
-        for (var i = 0; i < elts_cnt; i++) {
-            elts[i].checked = do_check;
-        }
-    }
+		if (elts_cnt) {
+			for (var i = 0; i < elts_cnt; i++) {
+				elts[i].checked = do_check;
+			}
+		} else {
+			elts.checked = do_check;
+		}
+	}
 
     return true;
 }
 
 function invertSelection(the_form, element_name) {
-    var elements    = document.forms[the_form].elements[element_name];
-    var count    = elements.length;
+	if (document.forms[the_form].elements[element_name]) {
+		var elements    = document.forms[the_form].elements[element_name];
+		var count    = elements.length;
 
-    if (count) {
-        for (var i = 0; i < count; i++) {
-            if(elements[i].checked == true) {
-                elements[i].checked = false;
-            } else {
-                elements[i].checked = true;
-            }
-        }
-    }
+		if (count) {
+			for (var i = 0; i < count; i++) {
+				if(elements[i].checked == true) {
+					elements[i].checked = false;
+				} else {
+					elements[i].checked = true;
+				}
+			}
+		} else {
+			if(elements.checked == true) {
+				elements.checked = false;
+			} else {
+				elements.checked = true;
+			}
+		}
+	}
 
     return true;
 }
