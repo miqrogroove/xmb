dc2aec0f9c81189f9f72a72b1afb0c65b0d5d458
jbriggs <>
2007-08-12 23:42:20 +0000

    fixed arrays implode
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code@238 4328ac3d-c443-4ce5-a636-224d0ca14295

--- source/stats.php
+++ source/stats.php
@@ -172,8 +172,9 @@ $viewmost = array();
 $query = $db->query("SELECT views, tid, subject FROM ".X_PREFIX."threads WHERE $restrict GROUP BY tid ORDER BY views DESC LIMIT 5");
 while ($views = $db->fetch_array($query)) {
     $view_subject = stripslashes(censor($views['subject']));
-    $viewmost[] = '<a href="viewthread.php?tid='.intval($views['tid']).'">'.$view_subject.'</a> ('.$views['views'].')<br />';
+    $viewmost[] = '<a href="viewthread.php?tid='.intval($views['tid']).'">'.$view_subject.'</a> ('.$views['views'].')';
 }
+$viewmost = implode('<br />', $viewmost);
 $db->free_result($query);
 
 // Get top 5 most replied to threads
@@ -181,8 +182,9 @@ $replymost = array();
 $query = $db->query("SELECT replies, tid, subject FROM ".X_PREFIX."threads WHERE $restrict GROUP BY tid ORDER BY replies DESC LIMIT 5");
 while ($reply = $db->fetch_array($query)) {
     $reply_subject = stripslashes(censor($reply['subject']));
-    $replymost[] = '<a href="viewthread.php?tid='.intval($reply['tid']).'">'.$reply_subject.'</a> ('.$reply['replies'].')<br />';
+    $replymost[] = '<a href="viewthread.php?tid='.intval($reply['tid']).'">'.$reply_subject.'</a> ('.$reply['replies'].')';
 }
+$replymost = implode('<br />', $replymost);
 $db->free_result($query);
 
 // Get last 5 posts
@@ -194,8 +196,9 @@ while ($last = $db->fetch_array($query)) {
     $lptime = gmdate($timecode, $last['lastpost'] + $adjTime);
     $thislast = "$lang[lpoststats] $lang[lastreply1] $lpdate $lang[textat] $lptime";
     $last_subject = stripslashes(censor($last['subject']));
-    $latest[] = '<a href="viewthread.php?tid='.intval($last['tid']).'">'.$last_subject.'</a> ('.$thislast.')<br/>';
+    $latest[] = '<a href="viewthread.php?tid='.intval($last['tid']).'">'.$last_subject.'</a> ('.$thislast.')';
 }
+$latest = implode('<br />', $latest);
 $db->free_result($query);
 
 // Get most popular forum
