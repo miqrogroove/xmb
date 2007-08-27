ecff43936b4581dbab92743afc3ec468ce41064c
jbriggs <>
2007-08-27 00:12:58 +0000

    updated source.
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code/source@334 4328ac3d-c443-4ce5-a636-224d0ca14295

--- lang/German.lang.php
+++ lang/German.lang.php
@@ -1,6 +1,5 @@
 <?php
-/* $Id: German.lang.php,v 1.1.2.9 2007/03/19 21:43:52 ajv Exp $ */
-/*
+/**
  * XMB 1.9.8 Engage Final
  *
  * Developed By The XMB Group
@@ -24,14 +23,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-    ----------------------------------------
-
-    Made by:    _DoE_ and LuCk3R
-    Language:   German (formal)
-    Last mod:   25th August 2007
-    Status:     beta
-*/
+ *
+ **/
 
 if (!defined('IN_CODE')) {
     exit ("Not allowed to run this file directly.");
