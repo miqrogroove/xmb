142caabfc9f76c9f186fe8449f3418a1eb61a9aa
mark <>
2007-11-14 20:19:37 +0000

    John has a better fix.
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code@461 4328ac3d-c443-4ce5-a636-224d0ca14295

--- source/include/online.inc.php
+++ source/include/online.inc.php
@@ -261,8 +261,6 @@ function url_to_text($url) {
         $location = $lang['onlineindex'];
     }
 
-    $location = shortenString($location);
-
     $location = html_entity_decode(str_replace('%20', '&nbsp;', htmlspecialchars($location)));
     $url = addslashes(trim($url));
 
