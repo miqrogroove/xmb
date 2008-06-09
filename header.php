51923fd8762e1699ca5092355fd87bd427b0eb48
miqrogroove <1371835+miqrogroove@users.noreply.github.com>
2008-06-09 23:55:46 +0000

    XMB 1.9.10 - XMB 1.9.10 - :D
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code/xmb19x/trunk/source@1092 4328ac3d-c443-4ce5-a636-224d0ca14295

--- header.php
+++ header.php
@@ -130,10 +130,10 @@ $versioncompany = 'The XMB Group';
 $copyright = '2001-2008';
 if ($show_full_info) {
     $alpha = '';
-    $beta = 'RC2';
+    $beta = '';
     $gamma = '';
     $service_pack = '';
-    $versionbuild = 20080608;
+    $versionbuild = 20080609;
     $versionlong = 'Powered by '.$versiongeneral.' '.$alpha.$beta.$gamma.$service_pack.''.(DEBUG === true ? ' (Debug Mode)' : '');
 } else {
     $alpha = '';
