4446b5c4649bc25d6aba9c935c60b4ca9af42539
miqrogroove <1371835+miqrogroove@users.noreply.github.com>
2008-12-17 08:06:29 +0000

    New patch for XMB 1.9.8 SP3 (Issue #228)
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code/xmb19x/branches/patchwork@1519 4328ac3d-c443-4ce5-a636-224d0ca14295

--- XMB_1_9_8_SP3/topicadmin.php
+++ XMB_1_9_8_SP3/topicadmin.php
@@ -133,6 +133,7 @@ switch($action) {
         nav($lang['textmovemethod1']);
         break;
     case 'getip':
+        $kill |= !X_ADMIN;
         nav($lang['textgetip']);
         break;
     case 'bump':
