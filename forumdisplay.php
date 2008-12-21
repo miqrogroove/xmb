2be6912939f7549e99ead48cd5dff7c7d7cc2a4b
miqrogroove <1371835+miqrogroove@users.noreply.github.com>
2008-12-21 08:45:27 +0000

    XMB 1.9.11 - Features - More status codes.
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code/xmb19x/trunk/source@1533 4328ac3d-c443-4ce5-a636-224d0ca14295

--- forumdisplay.php
+++ forumdisplay.php
@@ -165,11 +165,21 @@ validateTpp();
 validatePpp();
 
 $max_page = (int) ($forum['threads'] / $tpp) + 1;
-if ($page && $page >= 1 && $page <= $max_page) {
+if ($page > 1 && $page <= $max_page) {
     $start_limit = ($page-1) * $tpp;
-} else {
+} elseif ($page == 0 And !isset($_GET['page'])) {
     $start_limit = 0;
     $page = 1;
+} elseif ($page == 1) {
+    $newurl = preg_replace('/[^\x20-\x7e]/', '', $url);
+    $newurl = str_replace('&page=1', '', $newurl);
+    $newurl = substr($full_url, 0, -strlen($cookiepath)).$newurl;
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: '.$newurl);
+    exit;
+} else {
+    header('HTTP/1.0 404 Not Found');
+    error($lang['generic_missing']);
 }
 
 $cusdate = formInt('cusdate');
--- misc.php
+++ misc.php
@@ -372,11 +372,21 @@ switch($action) {
 
         $result = $db->result($db->query("SELECT COUNT(uid) FROM ".X_PREFIX."members WHERE lastvisit!=0"), 0);
         $max_page = (int) ($result / $memberperpage) + 1;
-        if ($page && $page >= 1 && $page <= $max_page) {
+        if ($page > 1 && $page <= $max_page) {
             $start_limit = ($page-1) * $SETTINGS['memberperpage'];
-        } else {
+        } elseif ($page == 0 And !isset($_GET['page'])) {
             $start_limit = 0;
             $page = 1;
+        } elseif ($page == 1) {
+            $newurl = preg_replace('/[^\x20-\x7e]/', '', $url);
+            $newurl = str_replace('&page=1', '', $newurl);
+            $newurl = substr($full_url, 0, -strlen($cookiepath)).$newurl;
+            header('HTTP/1.0 301 Moved Permanently');
+            header('Location: '.$newurl);
+            exit;
+        } else {
+            header('HTTP/1.0 404 Not Found');
+            error($lang['generic_missing']);
         }
 
         if ($order != 'username' && $order != 'postnum' && $order != 'status' && $order != 'location') {
