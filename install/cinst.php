dd96c4c4b11a401bede719ef4e26b03fb06620b5
daniel <>
2009-09-06 03:47:03 +0000

    Add "youtubestatus" in table settings
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code/xmb19x/trunk/source@2100 4328ac3d-c443-4ce5-a636-224d0ca14295

--- install/cinst.php
+++ install/cinst.php
@@ -525,7 +525,8 @@ $db->query("CREATE TABLE ".$tablepre."settings (
       `ip_banning` SET('on','off') NOT NULL DEFAULT 'off',
       `max_image_size` VARCHAR(9) NOT NULL DEFAULT '1000x1000',
       `max_thumb_size` VARCHAR(9) NOT NULL DEFAULT '200x200',
-      `schema_version` TINYINT UNSIGNED NOT NULL DEFAULT '3'
+      `schema_version` TINYINT UNSIGNED NOT NULL DEFAULT '3',
+      `youtubestatus` char(3) NOT NULL default 'on'
    ) TYPE=MyISAM
 ");
 // --------------------------------------------------------
