b54e5c38dfbb7130dd7684e534452ccd483630e5
miqrogroove <1371835+miqrogroove@users.noreply.github.com>
2020-10-22 13:54:10 +0000

    Merging 1.9.12 to Czech.
    git-svn-id: https://svn.code.sf.net/p/xmb-forum/code/xmb19x/branches/lang@2961 4328ac3d-c443-4ce5-a636-224d0ca14295

--- XMB_1_9_12/filesort.php
+++ XMB_1_9_12/filesort.php
@@ -1,12 +1,12 @@
 <?php
 
-$filepath = './Swedish.lang.php';
+$filepath = './Czech.lang.php';
 $file = file_get_contents( $filepath );
 $pos = strpos( $file, "\$lang['4spaces']" );
 $file = substr( $file, $pos );
 $lines = explode( "\n", $file );
 usort( $lines, 'mysql_simulator' );
-$file = implode( "\n", $lines );
+$file = implode( "\n", $lines ) . "\n";
 file_put_contents( $filepath . '.sort', $file );
 
 /**
