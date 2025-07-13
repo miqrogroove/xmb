8f4fd1fcc64b65ae20bd5c6332c2acba0941832e
miqrogroove <1371835+miqrogroove@users.noreply.github.com>
2025-07-13 12:17:20 -0400

    Fixed broken link in Who's Online

--- templates/misc_online_row_admin.php
+++ templates/misc_online_row_admin.php
@@ -4,7 +4,7 @@
 <td><?= $online['location'] ?></td>
 <td align="center">
 <a href="https://whois.domaintools.com/<?= $online['ip'] ?>" onclick="window.open(this.href); return false;" />W</a>
-<a href="https://www.net.princeton.edu/cgi-bin/traceroute.pl?target=<?= $online['ip'] ?>" onclick="window.open(this.href); return false;" />T</a>
+<a href="https://mxtoolbox.com/SuperTool.aspx?action=trace%3a<?= $online['ip'] ?>" onclick="window.open(this.href); return false;" />T</a>
 <a href="https://mxtoolbox.com/SuperTool.aspx?action=ptr%3a<?= $online['ip'] ?>" onclick="window.open(this.href); return false;">L</a>
 <a href="https://www.iptrackeronline.com/?ip_address=<?= $online['ip'] ?>" onclick="window.open(this.href); return false;">M</a> <?= $online['ip'] ?></a></td>
 </tr>
